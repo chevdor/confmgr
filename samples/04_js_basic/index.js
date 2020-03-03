c73e66c5e5117f87c291925b0d6977668f9351e8
Will <w.kopp@kigroup.de>
2020-03-03 18:13:27 +0100

    Fix and cleanup sample

--- samples/04_js_basic/index.js
+++ samples/04_js_basic/index.js
@@ -1,19 +1,8 @@
 const ConfigManager = require('confmgr').ConfigManager;
 const mySpecs = require('./configSpecs').default;
-// console.log('My specs:', JSON.stringify( mySpecs, null, 2));
 
 const config = ConfigManager.getInstance(mySpecs).getConfig();
 const valid = config.Validate();
 console.log(`Your config is${valid ? '' : ' NOT'} valid!`);
 
-// WARNING!
-// You should NOT display your config, especially in your logs, 
-// it may contain some secrets!
-// console.log('My Config:', JSON.stringify(config, null, 2));
-    
-// Instead, ask the ConfigManager to display it.
-// It will also look better
-// if (valid)
-config.DumpEnv(console.log);
-// else
-//   console.log('Your .env is not valid');
+config.Print({logger: console.log});
